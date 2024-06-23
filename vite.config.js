3da4a9fc5ebb96c42b57d4bafff5652aea733067
erikbernard <erikbrsilva@gmail.com>
2024-06-23 13:27:58 -0300

    change: add env and config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173, 
+    strictPort: true,
+    host: true,
+    origin: process.env.ORIGIN || "http://localhost:5173", 
+  },
+  preview: {
+    port: process.env.PREVIEW_PORT || 5173, 
+    strictPort: true,
+  },
+  define: {
+    "process.env.BASE_URL": JSON.stringify(process.env.BASE_URL),
+  },
+});
